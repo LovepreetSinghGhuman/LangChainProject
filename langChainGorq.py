adc5c63804ac1c5662345f90564ee2e8ef693c21
Lovepreet Singh Ghuman <lovepreet.singh1470@gmail.com>
2026-05-15 02:37:25 +0200

    restructering

--- langChainGorq.py
+++ langChainGorq.py
@@ -3,9 +3,10 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+path = "configs/"
 
 # Load API key from JSON file
-with open("groq_config.json", "r") as f:
+with open(path + "groq_config.json", "r") as f:
     config = json.load(f)
 api_key = config["groq_api_key"]
 
